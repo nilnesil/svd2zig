de6c21de3fc686f990c1d637c8f4196671c94997
nilnesil <2021270083@email.szu.edu.cn>
2023-09-12 23:22:28 +0800

    Change version of zig

--- build.zig
+++ build.zig
@@ -3,9 +3,12 @@ const Builder = std.build.Builder;
 const builtin = @import("builtin");
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("svd2zig", "src/main.zig");
-    exe.setBuildMode(mode);
+    const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "svd2zig",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .optimize = optimize,
+    });
 
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
--- src/svd.zig
+++ src/svd.zig
@@ -569,8 +569,8 @@ pub const Field = struct {
     }
 
     pub fn fieldResetValue(bit_start: u32, bit_width: u32, reg_reset_value: u32) u32 {
-        const shifted_reset_value = reg_reset_value >> @intCast(u5, bit_start);
-        const reset_value_mask = @intCast(u32, (@as(u33, 1) << @intCast(u6, bit_width)) - 1);
+        const shifted_reset_value = reg_reset_value >> @as(u5, @intCast(bit_start));
+        const reset_value_mask = @as(u32, @intCast((@as(u33, 1) << @as(u6, @intCast(bit_width))) - 1));
 
         return shifted_reset_value & reset_value_mask;
     }
@@ -779,12 +779,14 @@ fn bitWidthToMask(width: u32) u32 {
     const max_supported_bits = 32;
     const width_to_mask = blk: {
         comptime var mask_array: [max_supported_bits + 1]u32 = undefined;
-        inline for (mask_array) |*item, i| {
+        inline for (mask_array, 0..) |*item, i| {
             const i_use = if (i == 0) max_supported_bits else i;
             // This is needed to support both Zig 0.7 and 0.8
             const int_type_info =
                 if (@hasField(builtin.TypeInfo.Int, "signedness"))
-            .{ .signedness = .unsigned, .bits = i_use } else .{ .is_signed = false, .bits = i_use };
+                .{ .signedness = .unsigned, .bits = i_use }
+            else
+                .{ .is_signed = false, .bits = i_use };
 
             item.* = std.math.maxInt(@Type(builtin.TypeInfo{ .Int = int_type_info }));
         }
